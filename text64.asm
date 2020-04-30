23a3c28015ef30124157a4bf5f96e807535b0518
Giovanni Nunes <giovanni.nunes@gmail.com>
2020-04-30 10:34:10 -0300

    Add old files.

---
+++ text64.asm
@@ -0,0 +1,307 @@
+;
+;   TEXT64 version 1
+;
+
+;
+;   This is a experimental code and of course that has a lot of bugs!
+;   That's an idea that born in my mind during my lunch hour;
+;
+
+
+;	print usr("Giovanni")
+;   usr1(0) ' 0 = normal, 1 = inverse
+;	usr2(0)  '0 = CLS, 1 = SCRUP, 2 = SCRDW
+;   usr3(MAXLIN)
+;   usr4(MAXCOL)
+;
+
+
+;
+;	K I S S !
+;
+
+INCLUDE	./library/msx1bios.asm
+INCLUDE	./library/msx1variables.asm
+
+HTIMI:	equ 0xFD9F
+
+PAL:		equ 5						; 1/10s em 50Hz
+NTSC:		equ 6						; 1/10s em 60Hz
+
+			org 0xc000-7				; just a suggestion
+
+BINHEAD:	db 0xfe
+			dw BINSTART
+			dw BINSTOP
+			dw BINEXEC
+
+BINSTART:
+BINEXEC:	call INIGRP				; inicia SCR2
+
+			call TXT64CLRS				; limpa a tela
+
+			ld a,2						; X=2
+			ld (TXT64POSX),a
+			ld a,1
+			ld (TXT64POSY),a			; coloca o cursor em (2,1)
+
+			;~ ld b,24
+;~ LOOP0:		push bc
+			;~ call TXT64SCRL
+			;~ pop bc
+			;~ djnz LOOP0
+
+			call TXT64WRT
+
+LOOP:		jr LOOP
+
+MENSA:		db 'A',0
+
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+; *
+; *  Rotina de escrita em 64 colunas
+; *
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;
+TXT64WRT:	ld de,MENSA					; linha de texto (TEMP)
+
+TXT64WRT0:	ld a,(de)					; lê o primeiro caracter
+			cp 0
+			ret z						; NULL é fim da string, saio da rotina
+
+			and 0x7f					; esta versão é 7-bit only :-/
+
+			ld (TXT64CHAR),a			; armazeno o caracter a ser impresso
+
+			push de						; salva DE
+
+										; calcula a posição da VRAM e armazena
+			call TXT64VRMP				; em TXT64POSV e tanmbém a máscara a
+										; ser utilizada em TXT64EVEN
+
+		;	ld hl,TXT64POSV				; HL já vem assim da outra rotina
+			ld de,TXT64BYTE				; posição do buffer na RAM
+
+			ld b,8						; 8 bytes ao todo
+TXT64WRT1:	call RDVRM					; leio o byte na VRAM
+			ld (de),a					; armazena o byte lido
+			inc de						; incrementa DE
+			inc hl						; incrementa HL
+			djnz TXT64WRT1				; recupero o bloco da VRAM
+
+			;
+			; PARTE 3 - Calculo a posição do padrão do caracter a escrever
+			;
+			ld de,8
+			ld hl,0
+			ld a,(TXT64CHAR)
+			ld b,a						; valor do caracter lido
+
+TXT64WRT2:	add hl,de
+			djnz TXT64WRT2				; multiplica o caracter por 8
+
+			ex de,hl					; inverto DE com HL
+
+			ld hl,TXT64FONT				; a tabela de caracteres
+			add hl,de					; mais a posição do caracter
+
+			;
+			; PARTE 4 - Copio um padrão sobre o outro
+			;
+			ld b,8						; número de vezes a repetir
+
+			ld de,TXT64BYTE
+
+TXT64WRT3:	push bc						; salvo BC
+
+			ld a,(TXT64EVEN)			; carrego a máscara
+
+			and (hl)
+
+			ld a,(TXT64EVEN)			; recarrego a máscara
+			xor 255						; inverto os bits
+
+			ex de,hl					; inverto DE com HL
+
+			or (hl)						; aplica a máscara
+
+			ex de,hl					; (des)inverto DE com HL
+
+			push hl
+
+			ld hl,(TXT64POSV)
+			call WRTVRM					; aproveito o laço para
+			inc hl						; escrever na VRAM
+			ld (TXT64POSV),hl
+
+			pop hl
+
+			inc hl
+			inc de
+
+			pop bc
+
+			djnz TXT64WRT3
+
+			pop de
+
+			inc de
+
+			ld hl,TXT64POSX
+			inc (hl)					; avança X
+
+			pop de						; recupera DE
+
+			inc de						; vou para a próximo caracter
+
+			jr TXT64WRT0
+
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+; *
+; *  Calcula o endereço da VRAM a partir de X e Y
+; *
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;
+TXT64VRMP:	ld a,(TXT64POSX)			; posição de X
+
+			ld hl,TXT64EVEN
+			ld (hl),0xf0				; já assumo a máscara para X par
+
+			bit 0,a						; testo o bit 0 de A
+			cp 0
+			jr z,TXT64VRMP0				; se for par vou para TXT64VRAM0
+
+			ld (hl),0x0f				; altera para a máscara de ímpar
+
+TXT64VRMP0:	and 0x1e					; divido por 2 e faço um shift
+										; >1 = 000001111
+										; <1 = 000011110 (fica igual)
+			sla a						; <1 = 011111100
+			sla a						; <1 = 111111000
+
+			ld l,a						; L=A*8
+
+			ld a,(TXT64POSY)			; posição de Y
+
+			ld h,a						; H=A*256
+
+			ld (TXT64POSV),hl			; armazeno a posição da VRAM
+
+			ret							; saio da rotina
+
+
+
+;~ ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;~ ; *
+;~ ; *  Aplica a máscara para o novo caracter
+;~ ; *
+;~ ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;~ ;
+;~ TXT64MASK:	ld b,0xf0					; a princípo é par (XXXX0000)
+			;~ ld a,(TXT64CHAR)
+			;~
+			;~ cp 0						; é mesmo zero?
+			;~ jr TXT64MASK0
+			;~ ld b,0x0f					; então é ímpar (0000XXXX)
+			;~
+;~ TXT64MASK0:	ld c,b
+			;~
+			;~ ld b,8
+			;~ ld hl,TXT64BYTE				; endereço inicial do caracter
+			;~
+;~ TXT64MASK1:	ld a,(hl)					; recupera o byte
+			;~ and c						; aplica a máscara
+			;~ ld (hl),a					; armazena o byte com máscara
+			;~ inc hl						; nova posição de HL
+			;~ djnz TXT64MASK1
+			;~
+			;~ ret							; saio da rotina
+
+; passo 5 : incrementar o contador (é o mesmo caracter?)
+; passo 5.1 : passo 3
+; passo 6 ; retornar o bloco para a VRAM
+; passo 7 ; acabou a tela? (SCROLL)
+
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+; *
+; *  Limpa a tela - OK
+; *
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;
+TXT64CLRS:	ld a,0
+			ld bc,6144
+			ld hl,0
+			call FILVRM
+
+			ld a,0xf4
+			ld bc,6144
+			ld hl,8192
+			call FILVRM
+
+			xor a
+			ld (TXT64POSX),a			; cursor em (0,0)
+			ld (TXT64POSY),a
+
+			ret							; saio da rotina
+
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+; *
+; *  Rotaciona a tela para cima - OK
+; *
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;
+TXT64SCRL:	ld b,23
+			ld de,0						; linha 0
+			ld hl,256					; linha 1
+
+TXT64SCRL0:	push bc						; guarda BC
+
+			ld (TXT64SCRL1+1),hl
+			ld (TXT64SCRL2+1),de
+
+			inc d
+			inc h
+
+			push de
+			push hl
+
+			ld bc,256
+TXT64SCRL1:	ld hl,0
+			ld de,TXT64BUFF
+			call LDIRMV					; copia a linha da tela na RAM
+
+			ld bc,256
+TXT64SCRL2:	ld de,0
+			ld hl,TXT64BUFF
+			call LDIRVM					; copia a linha para a VRAM
+
+			pop hl						; recupera HL
+			pop de						; recupera DE
+			pop bc						; recupera BC
+			djnz TXT64SCRL0
+
+			ld a,0
+			ld bc,256
+			ld hl,23*256
+			call FILVRM				; limpa a última linha da tela
+
+			ret							; saio da rotina
+
+TXT64CHAR:	db 0						; Caracter a ser impresso
+TXT64EVEN:	db 0						; É ímpar?
+TXT64POSX:	db 0						; posição X do cursor
+TXT64POSY:	db 0						; posição Y do cursor
+TXT64POSV:	dw 0						; posição na VRAM
+
+TXT64BYTE:	db 0,0,0,0,0,0,0,0			; buffer de impressão
+
+TXT64FONT:
+            include	"font64.asm"        ; 1024KiB
+
+TXT64BUFF:
+			rept 256
+			db 0						; buffer do programa
+			endm
+
+BINSTOP:
+;	MSX Rulez a Lot!
---
+++ txt64.asm
@@ -0,0 +1,402 @@
+;   
+;   TXT64 version 1
+;   
+
+;
+;   This is a experimental code and of course that has a lot of bugs!
+;   That's an idea that born in my mind during my lunch hour;
+;
+
+;  5 BLOAD "TX64DRV.BIN",r
+; 10 COLOR 15,4,0:SCREEN 2
+; 20 POKE WIDTH,64:POKE HEIGHT,24
+; 30 A%=USR0(0)'INITSCR
+; 40 PRINT USR0("Teste") 
+; 50 LOCATE 64,2:PRINT USR0("Agora aqui")
+; 60 POKE MODE,1 USR0(256)
+; 70 LOCATE 64,3:PRINT USR0("Invertido agora")
+; 80 POKE MODE,1 USR0(512)
+;
+; USR0(0) ; limpa a tela : 0 = limpa a tela; 1-24 = número de linhas ; 64
+; USR1(0) ; print sem NL
+; USR2(0) ; print com NL
+;
+;
+
+
+INCLUDE	./library/msx1bios.asm
+INCLUDE	./library/msx1variables.asm
+
+HTIMI:		equ 0xFD9F
+
+PAL:		equ 5						; 1/10s em 50Hz
+NTSC:		equ 6						; 1/10s em 60Hz
+
+;
+; só irei implementar o ajuste do número de linhas
+;
+WIDTH:		equ 64
+HEIGHT:		equ 10
+MAXHEIGHT:	equ HEIGHT-1
+
+			org 0xc000-7				; just a suggestion
+			
+BINHEAD:	db 0xfe
+			dw BINSTART
+			dw BINSTOP
+			dw BINEXEC
+			
+BINSTART:
+BINEXEC:	call INIGRP					; inicia SCR2
+			
+			call TXT64CLS				; limpa a tela
+			
+			ld a,0xaa
+			ld bc,6144
+			ld hl,0
+			call FILVRM
+			
+			ld a,0						; X=2
+			ld (TXT64POSX),a
+			
+			ld a,0
+			ld (TXT64POSY),a			; coloca o cursor em (2,1)
+			
+			ld a,0
+			ld (TX64MODE),a			; invertido?
+			
+			ld b,64
+			
+LOOP0:		push bc
+			
+			call TXT64WRT
+			
+			ld a,(TX64MODE)
+			xor 1
+			ld (TX64MODE),a
+			
+			pop bc
+			djnz LOOP0
+			
+LOOP:		jr LOOP
+
+MENSA:		db ' A routine that simulates a 64x24 text mode that works fine '
+			db 'on MSX, MSX2, MSX2+ and MSX Turbo R! Now with normal and '
+			db 'inverse text! :-) ',7,0
+
+; Rotinas:
+; TXT64SCR (gerencia a tela)
+;	TXT64CR
+;	TXT64NL
+;	TXT64ESC
+;
+
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+; *
+; *  Rotina de escrita em 64 colunas
+; *
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;
+TX64WRIT:	ld de,MENSA					; provisório
+			
+TX64WRIT0:	ld a,(de)
+			
+			cp 0						; se for NULL saio da rotina
+			ret z
+			
+			push de						; salvo DE
+			
+			cp 7
+			jp z,TX64_Beep
+			
+			cp 8						; volta um caracter
+			jp z,TX64_Backspace
+			
+			cp 9						; tabulação
+			jp z,TX64_Tabspace
+			
+			;~ cp 10						; avanço de linha
+			;~ jp z,TX64_Linefeed
+			;~ 
+			;~ cp 11						; cursor em home (0,0)
+			;~ jp z,TX64_Home
+			;~ 
+			;~ cp 12
+			;~ jp z,TX64_ClearScreen		; limpa a tela
+			;~ 
+			;~ cp 13
+			;~ jp z,TX64_CarriageReturn	; retorno de carro
+			;~
+			;~ cp 27
+			;~ jp z,TX64_EscapeCodes
+			;~ 
+			;~ cp 28
+			;~ jp z,TX64_CsrRight
+			;~ 
+			;~ cp 29
+			;~ jp z,TX64_CsrLeft
+			;~ 
+			;~ cp 30
+			;~ jp z,TX64_CsrUp
+			;~ 
+			;~ cp 31
+			;~ jp z,TX64_CsrDown
+			
+			jp TX64CHPUT				; rotina de impressão de caracter
+			
+TX64WRIT1:	pop de						; recupera DE
+			
+			inc de						; vou para a próximo caracter
+			
+			jp TXT64WRIT0
+
+
+;
+; Beep
+;
+TX64_Beep:
+			call BEEP
+			jp TX64WRT1
+
+;
+; Backspace
+;
+TX64_Backspace:
+			ld a,(TX64POSX)
+			dec a
+			cp -1
+			jr nz,
+			xor a
+TX64_Backspace1:
+			ld (TX64POSX)
+			jp TX64WRIT1
+
+;
+; Tabulating
+;
+TX64_Tabspace:
+			ld a,(TX64POSX)
+			and 0xf8
+			rept 8
+			inc a
+			endm
+			
+
+
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+; *
+; *  Imprime um caracter tentando ser compatível com a rotina CHPUT da BIOS 
+; *
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;
+TX64CHPUT:								; preciso salvar todo mundo!
+			
+			and 0x7f					; por enquanto é apenas 7-bit
+			
+			ld (TX64CHAR),a				; armazeno o caracter a imprimir
+			
+			ld a,(CSRX)					; Lê a posição X do cursor (CSRX)
+			dec a						; A=A-1
+			
+			push af
+			and 1						; só preciso do primeiro bit
+			ld (TX64EVEN),a				; 0 = coluna par, 1 = coluna ímpar
+			pop af
+			
+			srl a						; >1 = 000111111
+			sla a						; <1 = 001111110
+			sla a						; <1 = 011111100
+			sla a						; <1 = 111111000
+			
+			ld l,a						; L= (X\2)*8
+			
+			ld a,(CSRY)					; Lê a posição Y do cursor (CSRY)
+			dec a						; A=A-1
+			ld h,a						; H=Y*256
+			
+			ld (TX64POSV),hl			; armazeno a posição da VRAM
+			
+			ld de,8
+			ld hl,TX64FONT				; aponto a tabela de caracteres em 4x8
+			
+			ld a,(TX64CHAR)				; o caracter que será impresso
+			
+			ld b,a						; armazeno em B
+			
+TX64CHPUT1:	add hl,de
+			djnz TX64CHPUT1				; multiplica o caracter por 8
+			
+			ld (TX64POSC),hl			; salvo a posição do caracter
+			
+			ex de,hl					; inverto DE com HL
+			
+			ld hl,(TX64POSV)			; HL agora contém o endereço da VRAM
+			
+			ld b,8						; os 8 bytes do caracter
+			
+TX64CHPUT2:	push bc
+			
+			call RDVRM					; lê o byte na VRAM
+			
+			ex af,af'
+			
+			ld a,(TX64EVEN)				; par ou ímpar?
+			cp 1
+			jr z,TX64CHPUT3
+			
+			ld a,0x0f					; máscara para par
+			jr TX64CHPUT4
+			
+TX64CHPUT3:	ld a,0xf0					; máscara para ímpar
+			
+TX64CHPUT4:	ld (TX64_MASK1+1),a
+			xor 255						; inverto a máscara
+			ld (TX64_MASK2+1),a
+			
+			ld a,(TX64MODE)				; texto normal ou invertido?
+			cp 1
+			jr nz,TX64CHPUT5
+			
+			ld a,255					; A=11111111
+			jr TX64CHPUT6
+			
+TX64CHPUT5:	xor a						; A=00000000
+			
+TX64CHPUT6:	ld (TX64_MODE1+1),a
+			
+			ex af,af'
+			
+TX64_MASK1:	and 0xff					; apago o espaço do caracter
+			
+			ld b,a
+			ld a,(de)					; leio o endereço apontado em DE
+			
+TX64_MODE1:	xor 255
+			
+TX64_MASK2:	and 0xff					; seleciono apenas o caracter par
+			
+			or b						; aplico o novo caracter
+			
+			call WRTVRM					; escrevo na VRAM
+			
+			inc de						; nova posição da fonte
+			inc hl						; nova posição da VRAM
+			
+			pop bc						; recupero BC
+			
+			djnz TX64CHPUT2
+			
+			; ------------------ fim da impressão
+			
+			ld a,(TXT64POSX)			; leio a posição da coluna
+			inc a						; avanço para o próximo caracter
+			cp WIDTH					; é 64?
+			jr nz,TXT64CHR5				; senão segue para WRT5
+			xor a						; zero o número da coluna
+			ld (TXT64POSX),a			; armazeno a coluna
+			
+			ld a,(TXT64POSY)			; recupero a linha
+			inc a						; incremento a nova linha
+			cp HEIGHT					; é 24?
+			jr nz, TXT64CHR6			; senão segue para WRT6
+			ld a,MAXHEIGHT				; valor máximo da linha
+			ld (TXT64POSY),a			; armazeno a nova posição
+			
+			call TXT64SCRL				; faço scroll da tela
+			jr TXT64CHR7
+			
+TXT64CHR5:	ld (TXT64POSX),a			; armazeno a posição da coluna
+			jr TXT64CHR7 
+			
+TXT64CHR6:	ld (TXT64POSY),a
+			
+TXT64CHR7:	jp TXT64WRT1
+
+
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+; *
+; *  Limpa a tela - OK
+; *
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;
+TXT64CLS:	ld a,0
+			ld bc,6144
+			ld hl,0
+			call FILVRM
+			
+			ld a,0xf4					; é gambi, depois sofistico
+			ld bc,6144					;
+			ld hl,8192					;
+			call FILVRM					;
+			
+			xor a
+			ld (TXT64POSX),a			; cursor em (0,0)
+			ld (TXT64POSY),a
+			
+			ret							; saio da rotina
+
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+; *
+; *  Rotaciona a tela uma linha para cima
+; *
+; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;
+TXT64SCRL:	ld b,(HEIGHT)
+			ld de,0						; linha 0
+			ld hl,256					; linha 1
+			
+TXT64SCRL0:	push bc						; guarda BC
+			
+			ld (TXT64SCRL1+1),hl
+			ld (TXT64SCRL2+1),de
+			
+			inc d						; é igual a DE=DE+256
+			inc h						; é igual a HL=HL+256
+			
+			push de
+			push hl
+			
+			ld bc,256
+TXT64SCRL1:	ld hl,0
+			ld de,TXT64BUFF
+			call LDIRMV					; copia a linha da tela na RAM
+			
+			ld bc,256
+TXT64SCRL2:	ld de,0
+			ld hl,TXT64BUFF
+			call LDIRVM					; copia a linha para a VRAM
+			
+			pop hl						; recupera HL
+			pop de						; recupera DE
+			pop bc						; recupera BC
+			djnz TXT64SCRL0
+			
+			ld a,0
+			ld bc,256
+			ld hl,MAXHEIGHT*256
+			call FILVRM					; limpa a última linha da tela
+			
+			ret							; saio da rotina
+
+; - variáveis do programa
+			
+TX64CHAR:	db 0						; Caracter a ser impresso
+
+TX64EVEN:	db 0						; É ímpar?
+TX64MODE:	db 0						; 0 = normal, 1 = invertido
+
+TX64POSX:	db 0						; posição X do cursor
+TX64POSY:	db 0						; posição Y do cursor
+
+TX64POSV:	dw 0						; posição na VRAM
+TX64POSC:	dw 0						; posição do padao de caracter
+			
+TXT64FONT:	inclide	./font64.asm		; 1024KiB
+			
+TXT64BUFF:
+			rept 256
+			db 0						; buffer de linha do scroll
+			endm
+
+BINSTOP:
+;	MSX Rulez a Lot!
